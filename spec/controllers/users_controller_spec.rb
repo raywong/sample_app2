0f04f7053ae73a5d7ca4937851defc2494639ec1
Ray Wong <wongyuanrui@gmail.com>
2011-08-09 18:08:44 -0400

    Added user microposts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -90,7 +90,16 @@ describe UsersController do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
     end
+
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.should have_selector("span.content", :content => mp2.content)
+    end
   end
+
 
   describe "GET 'new'" do
     it "should be successful" do
@@ -98,8 +107,6 @@ describe UsersController do
       response.should be_success
     end
   
-
-
   it "should have the right title" do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
